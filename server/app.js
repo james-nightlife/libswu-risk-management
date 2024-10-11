0333df56cdf6a34a65b99f0e37f78a285d127443
james-nightlife <sutthiphong.kan@outlook.com>
2024-10-11 10:05:24 +0700

    modified

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.get('/view', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
